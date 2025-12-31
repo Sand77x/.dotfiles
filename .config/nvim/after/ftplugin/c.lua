79960065e9682f300e75ccfa9dcd784717193ae6
sand77x <sand77x@gmail.com>
2025-12-31 17:01:40 +0800

    initial commit

---
+++ .config/nvim/after/ftplugin/c.lua
@@ -0,0 +1,4 @@
+vim.api.nvim_buf_set_keymap(0, 'n', '<leader>fo', '<nop>', {
+    noremap = true,
+    silent = true,
+})
---
+++ .config/nvim/lua/kickstart/plugins/lint.lua
@@ -0,0 +1,47 @@
+return {
+
+    { -- Linting
+        'mfussenegger/nvim-lint',
+        event = { 'BufReadPre', 'BufNewFile' },
+        config = function()
+            local lint = require('lint')
+            -- lint.linters_by_ft['clojure'] = { 'clj-kondo' }
+            -- lint.linters_by_ft['dockerfile'] = { 'hadolint' }
+            -- lint.linters_by_ft['inko'] = { 'inko' }
+            -- lint.linters_by_ft['janet'] = { 'janet' }
+            lint.linters_by_ft['json'] = { 'jsonlint' }
+            lint.linters_by_ft['markdown'] = nil
+            -- lint.linters_by_ft['rst'] = { 'vale' }
+            -- lint.linters_by_ft['ruby'] = { 'ruby' }
+            -- lint.linters_by_ft['terraform'] = { 'tflint' }
+            lint.linters_by_ft['text'] = nil
+
+            -- You can disable the default linters by setting their filetypes to nil:
+            -- lint.linters_by_ft['clojure'] = nil
+            -- lint.linters_by_ft['dockerfile'] = nil
+            -- lint.linters_by_ft['inko'] = nil
+            -- lint.linters_by_ft['janet'] = nil
+            -- lint.linters_by_ft['json'] = nil
+            -- lint.linters_by_ft['markdown'] = nil
+            -- lint.linters_by_ft['rst'] = nil
+            -- lint.linters_by_ft['ruby'] = nil
+            -- lint.linters_by_ft['terraform'] = nil
+            -- lint.linters_by_ft['text'] = nil
+
+            -- Create autocommand which carries out the actual linting
+            -- on the specified events.
+            local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+            vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+                group = lint_augroup,
+                callback = function()
+                    -- Only run the linter in buffers that you can modify in order to
+                    -- avoid superfluous noise, notably within the handy LSP pop-ups that
+                    -- describe the hovered symbol using Markdown.
+                    if vim.bo.modifiable then
+                        lint.try_lint()
+                    end
+                end,
+            })
+        end,
+    },
+}
